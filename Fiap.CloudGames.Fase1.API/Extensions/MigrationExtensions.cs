52bcfbef6c9735d6e1549dfdaabb865657afa884
Leonardo <leonardogbf@gmail.com>
2025-07-27 00:41:50 -0300

    Adicionando suporte ao Docker com o serviço da aplicação, BD Postgres e um volume para os arquivos do BD. Execução automática das migrations adicionado para modo debug.

---
+++ Fiap.CloudGames.Fase1.API/Extensions/MigrationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Fiap.CloudGames.Fase1.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fiap.CloudGames.Fase1.API.Extensions
+{
+    public static class MigrationExtensions
+    {
+        public static void ApplyMigrations(this IApplicationBuilder app)
+        {
+            using IServiceScope scope = app.ApplicationServices.CreateScope();
+
+            using ApplicationDbContext dbContext = 
+                scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            dbContext.Database.Migrate();
+        }
+    }
+}
